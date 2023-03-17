22fc949c07eb9ef7968688f257759e5a39f4ba68
A.Wael <96313471+AbdelrahmanWael2@users.noreply.github.com>
2023-03-17 17:13:46 +0200

    Create Main.java

---
+++ Trees/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        RedBlack<Integer> rb = new RedBlack<Integer>(10);
+      //get height
+      //get size
+      //insert
+      //search will return the node
+      //delete not complete
+    }
+}
